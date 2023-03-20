b229544ae2ba65ee321326ae289a37ca93f555b0
Gustavo Lins <gnlins@gmail.com>
2023-03-20 19:29:37 -0300

    feat:Creating form.html

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def form(request):
+    return render(request, 'form.html')
