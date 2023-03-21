668404102257544feeca1b27027b22529c3cf28c
Gustavo Lins <gnlins@gmail.com>
2023-03-21 19:57:17 -0300

    feat:Create and read done

--- app/views.py
+++ app/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import CarrosForm
+from app.models import Carros
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    data = {}
+    data['db'] = Carros.objects.all()
+    return render(request,'index.html', data)
 
 def form(request):
     data = {}
     data['form'] = CarrosForm()
     return render(request, 'form.html', data)
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    return render(request, 'view.html', data)
