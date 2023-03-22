8c07fe81680020fa57da20336ec39ee9ead75d93
Gustavo Lins <gnlins@gmail.com>
2023-03-22 08:23:58 -0300

    feat:Update db

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form, create, view
+from app.views import home, form, create, view, edit, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('form/',form, name='form'),
     path('create/',create,name='create'),
-    path('view/<int:pk>/', view, name='view')
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update')
 ]
