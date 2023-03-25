6979a4bc6631320a808a44846b1bd67decc8fd4c
Gustavo Lins <gnlins@gmail.com>
2023-03-25 11:46:50 -0300

    feat: making pagination and search

--- app/views.py
+++ app/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from app.forms import CarrosForm
 from app.models import Carros
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
     data = {}
-    data['db'] = Carros.objects.all()
+    search = request.GET.get('search')
+    if search:
+        data['db'] = Carros.objects.filter(modelo__icontains=search)
+    else:
+        data['db'] = Carros.objects.all()
+
+
+    # all =  Carros.objects.all()
+    # paginator = Paginator(all,2)
+    # pages = request.GET.get('page')
+    # data['db'] = paginator.get_page(pages)
     return render(request,'index.html', data)
 
 def form(request):
